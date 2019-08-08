69e1fb7aa516619196dca21c9da2fd9d85e7848a
Nick Mertens <nick@thecrypt.co.nz>
2019-08-08 17:36:39 +1200

    Add vue-loader and babel-loader, plus Vue CSS support

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
   entry: "./src/index.js",
@@ -12,13 +14,31 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        include: /node_modules\/inkjs\/dist\/ink-es2015\.js/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          isDev ? "vue-style-loader" : MiniCSSExtractPlugin.loader,
+          { loader: "css-loader", options: { sourceMap: isDev } }
+        ]
       }
     ]
   },
   plugins: [new VueLoaderPlugin()],
   resolve: {
+    extensions: [".js", ".vue"],
     alias: {
-      vue: "vue/dist/vue.js"
+      vue$: isDev ? "vue/dist/vue.js" : "vue/dist/vue.min.js",
+      inkjs$: "inkjs/dist/ink.js"
     }
-  }
+  },
+  devtool: "none"
 };
