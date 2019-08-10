0cc57dadc6c048d9d4f85eec63cbfcd1b680bdae
Nick Mertens <nick@thecrypt.co.nz>
2019-08-10 16:15:46 +1200

    Add contact links

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -2,17 +2,19 @@
   <article class="lr-tile" v-bind:key="item.id">
     <h1>{{ item.title }}</h1>
     <p>{{ item.description }}</p>
-    <a v-if="item.url" :href="item.url" target="_blank" class="lr-tile-button">
-      <span class="lr-tileButton-link">{{ extractDomainFromUrl(item.url) }}</span>
-      <span class="lr-tileButton-icon">
-        <i class="lr-icon lr-icon--externalLink" />
-      </span>
-    </a>
-    <a v-else v-on:click="onClick(item)" class="lr-tile-button">
-      <span class="lr-tileButton-icon">
-        <i class="lr-icon lr-icon--play" />
-      </span>
-    </a>
+    <div class="lr-tile-action">
+      <a v-if="item.url" :href="item.url" target="_blank" class="lr-tile-button">
+        <span class="lr-tileButton-link">{{ extractDomainFromUrl(item.url) }}</span>
+        <span class="lr-tileButton-icon">
+          <i class="lr-icon lr-icon--externalLink" />
+        </span>
+      </a>
+      <a v-else v-on:click="onClick(item)" class="lr-tile-button">
+        <span class="lr-tileButton-icon">
+          <i class="lr-icon lr-icon--play" />
+        </span>
+      </a>
+    </div>
   </article>
 </template>
 
