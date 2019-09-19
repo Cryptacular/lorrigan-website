243261aa1719bc724a333b823a4adf718717715c
Nick Mertens <nick@thecrypt.co.nz>
2019-09-19 17:31:00 +1200

    Move Pug rendering to API endpoint. Add routes for pages.

--- build.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs"),
-  path = require("path"),
-  pug = require("pug");
-
-var html = pug.renderFile("views/index.pug", {
-  year: new Date().getFullYear()
-});
-
-fs.writeFileSync(path.resolve(__dirname, "public/index.html"), html);
