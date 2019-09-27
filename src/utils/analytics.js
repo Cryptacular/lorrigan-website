2108e25da4aea2fc646fe9fe39c18587d96271a7
Nick Mertens <nick@thecrypt.co.nz>
2019-09-27 14:06:31 +1200

    Fix custom Analytics tracking events

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,15 +1,36 @@
+const isDevEnvironment = window.location.hostname === "localhost";
+
 export const trackEvent = (category, action, label = null, value = null) => {
-  if (typeof ga === "undefined") {
+  if (typeof gtag === "undefined") {
     return;
   }
 
-  ga("send", "event", category, action, label, value);
+  if (isDevEnvironment) {
+    console.log("Event tracked:", category, action, label, value);
+    return;
+  }
+
+  gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value
+  });
 };
 
 export const trackTiming = (category, action, value, label = null) => {
-  if (typeof ga === "undefined") {
+  if (typeof gtag === "undefined") {
     return;
   }
 
-  ga("send", "timing", category, action, label, value);
+  if (isDevEnvironment) {
+    console.log("Event tracked:", category, action, label, value);
+    return;
+  }
+
+  gtag("event", "timing_complete", {
+    name: action,
+    value: value,
+    event_category: category,
+    event_label: label
+  });
 };
